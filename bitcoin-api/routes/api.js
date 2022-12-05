cb5d472a7208eabac0856a000be6f0a0ff12478f
Dan Hayes <github@danhayes.simplelogin.com>
2022-12-05 18:39:04 +0000

    add getblock count route

--- bitcoin-api/routes/api.js
+++ bitcoin-api/routes/api.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 const USER = process.env.RPC_USER;
 const PASS = process.env.RPC_PASSWORD;
+const NODEIP = process.env.LOCAL_NODE_IP;
 
 const headers = {
   "content-type": "text/plain;"
@@ -15,7 +16,24 @@ const headers = {
 router.get("/test", (req, res) => res.json({ msg: "backend works" }));
 
 
-// ... routes will go here
+
+router.get("/getblockcount", (req, res) => {
+    var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockcount","params":[]}`;
+    var options = {
+      url: `http://${USER}:${PASS}@192.168.1.137:8332/`,
+      method: "POST",
+      headers: headers,
+      body: dataString
+    };
+    
+    callback = (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        const data = JSON.parse(body);
+        res.send(data);
+      }
+    };
+    request(options, callback);
+  });
 
 
 module.exports = router;
