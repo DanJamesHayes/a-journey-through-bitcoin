e394c610e314712227352834235e8cfd37d90b2e
Dan Hayes <github@danhayes.simplelogin.com>
2022-12-14 21:24:07 +0000

    Add routes to api.js

--- bitcoin-api/routes/api.js
+++ bitcoin-api/routes/api.js
@@ -16,7 +16,7 @@ const headers = {
 router.get("/test", (req, res) => res.json({ msg: "backend works" }));
 
 
-
+// getblockcount route
 router.get("/getblockcount", (req, res) => {
     var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockcount","params":[]}`;
     var options = {
@@ -35,5 +35,230 @@ router.get("/getblockcount", (req, res) => {
     request(options, callback);
   });
 
+
+// getbestblockhash route
+router.get("/getbestblockhash", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getbestblockhash","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@${NODEIP}:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+
+// getconnectioncount route
+router.get("/getconnectioncount", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getconnectioncount","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@${NODEIP}:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+
+// getdifficulty route
+router.get("/getdifficulty", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getdifficulty","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@${NODEIP}:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+// getblockchaininfo route
+router.get("/getblockchaininfo", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockchaininfo","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@${NODEIP}:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+
+// getmininginfo route
+router.get("/getmininginfo", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getmininginfo","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@${NODEIP}:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+
+// getpeerinfo
+router.get("/getpeerinfo", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getpeerinfo","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@${NODEIP}:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+
+// getrawmempool route
+router.get("/getrawmempool", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getrawmempool","params":[]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@${NODEIP}:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+
+// getblock route
+router.get("/getblock/:hash", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblock","params":["${
+    req.params.hash
+  }"]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@${NODEIP}:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+// getblockhash route
+router.get("/getblockhash/:index", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getblockhash","params":[${
+    req.params.index
+  }]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@127.0.0.1:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+
+// getrawtransaction route
+router.get("/getrawtransaction/:id", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"getrawtransaction","params":["${
+    req.params.id
+  }"]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@${NODEIP}:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
+
+
+// decoderawtransation route
+router.get("/decoderawtransaction/:hex", (req, res) => {
+  var dataString = `{"jsonrpc":"1.0","id":"curltext","method":"decoderawtransaction","params":["${
+    req.params.hex
+  }"]}`;
+  var options = {
+    url: `http://${USER}:${PASS}@${NODEIP}:8332/`,
+    method: "POST",
+    headers: headers,
+    body: dataString
+  };
+
+  callback = (error, response, body) => {
+    if (!error && response.statusCode == 200) {
+      const data = JSON.parse(body);
+      res.send(data);
+    }
+  };
+  request(options, callback);
+});
 
 module.exports = router;
